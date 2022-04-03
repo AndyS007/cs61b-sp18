f6d3ce609db4e343b274eff209f1096b90e7e1d3
andy <445998251@qq.com>
2022-04-03 15:56:12 +0800

    finish LinkdedListDeque

---
+++ proj1a/src/LinkedListDeque.java
@@ -0,0 +1,136 @@
+
+public class LinkedListDeque<T> implements Deque<T> {
+    //inner class
+    private class ListNode {
+
+        T item;
+        ListNode prev;
+        ListNode next;
+
+        ListNode(T item, ListNode prev, ListNode next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        ListNode() {
+
+        }
+    }
+
+    private int size;
+    private ListNode sentinel;
+
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new ListNode();
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        ListNode temp = sentinel.next;
+        sentinel.next = new ListNode(item, sentinel, temp);
+        temp.prev = sentinel.next;
+        size += 1;
+    }
+
+    @Override
+    public void addLast(T item) {
+        ListNode temp = sentinel.prev;
+        sentinel.prev = new ListNode(item, temp, sentinel);
+        temp.next = sentinel.prev;
+        size += 1;
+
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        ListNode temp = sentinel.next;
+        while (temp != sentinel) {
+            System.out.print(temp.item + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    @Override
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        ListNode temp = sentinel.next;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = temp.prev;
+        size -= 1;
+        return temp.item;
+    }
+
+    @Override
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        ListNode temp = sentinel.prev;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = temp.next;
+        size -= 1;
+        return temp.item;
+    }
+    @Override
+    public T get(int index) {
+        ListNode temp = sentinel;
+        if (index >= size) {
+            return null;
+        }
+        if (index <= size / 2) {
+            for (int i = -1; i < index; i += 1) {
+                temp = temp.next;
+            }
+        } else {
+            for (int i = size; i > index; i -= 1) {
+                temp = temp.prev;
+            }
+        }
+        return temp.item;
+    }
+    @Override
+    public boolean equals(Object x) {
+        if (!(x instanceof LinkedListDeque)) {
+            return false;
+        }
+        LinkedListDeque<T> list = (LinkedListDeque<T>) x;
+        ListNode a = this.sentinel.next;
+        ListNode b = list.sentinel.next;
+        while (a != this.sentinel && b != list.sentinel) {
+            if (a.item != b.item) {
+                return false;
+            }
+            a = a.next;
+            b = b.next;
+        }
+        return a == this.sentinel && b == list.sentinel;
+    }
+
+    public static <K> LinkedListDeque<K> of(K... args) {
+        LinkedListDeque<K> result = new LinkedListDeque<>();
+        for (K arg : args) {
+            result.addLast(arg);
+        }
+        return result;
+    }
+
+}
+
---
+++ proj1a/src/LinkedListDequeJunitTest.java
@@ -0,0 +1,61 @@
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+public class LinkedListDequeJunitTest {
+    @Test(timeout = 1000)
+    public void testEquals(){
+        LinkedListDeque<Integer> listOne = new LinkedListDeque<>();
+        LinkedListDeque<Integer> listTwo = new LinkedListDeque<>();
+        listOne.addFirst(2);
+        listOne.addFirst(3);
+        listOne.addFirst(4);
+
+        listTwo.addLast(4);
+        listTwo.addLast(3);
+        listTwo.addLast(2);
+        listOne.printDeque();
+        listTwo.printDeque();
+        assertEquals(listOne, listTwo);
+
+    }
+    @Test
+    public void testEqualsNull(){
+        LinkedListDeque<Integer> listOne = new LinkedListDeque<>();
+        LinkedListDeque<Integer> listTwo = new LinkedListDeque<>();
+
+        assertEquals(listOne, listTwo);
+    }
+
+    @Test
+    public void testListOf() {
+        LinkedListDeque<Integer> a = LinkedListDeque.of(1, 2, 3, 4, 5);
+        LinkedListDeque<Integer> b = LinkedListDeque.of();
+        LinkedListDeque<String> string = LinkedListDeque.of("hello", "world", "!");
+        assertEquals(true, b.isEmpty());
+        assertEquals(0, b.size());
+        assertEquals(3, string.size());
+    }
+
+    @Test
+    public void testRemoveFirstAndLast() {
+        LinkedListDeque<Integer> a = LinkedListDeque.of(1, 2, 3 ,4, 5);
+        a.removeFirst();
+        a.removeLast();
+        a.printDeque();
+        LinkedListDeque<Integer> exp = LinkedListDeque.of(2, 3, 4);
+        assertEquals(a, exp);
+    }
+
+    @Test
+    public void testGet() {
+        LinkedListDeque<Integer> a = LinkedListDeque.of(1, 2, 3 ,4, 5);
+        assertEquals(Integer.valueOf(1), a.get(0));
+        assertEquals(Integer.valueOf(2), a.get(1));
+        assertEquals(Integer.valueOf(4), a.get(3));
+        assertEquals(Integer.valueOf(5), a.get(4));
+        a.printDeque();
+
+    }
+}
