2c2c21a7097acef01a1a47be3eec3dac4ccdd75b
andy <445998251@qq.com>
2022-04-21 19:33:19 +0800

    finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,88 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    boolean[][] grid;
+    // change to public when everything done.
+    WeightedQuickUnionUF uf;
+    WeightedQuickUnionUF ufTop;
+
+    private int numberOfOpenSites;
+    int visualTopPoint;
+    int visualBottomPoint;
+    int N;
+    public static void main(String[] args) {
+        Percolation p = new Percolation(5);
+
+    }
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N must > 0");
+        }
+        this.N = N;
+        grid = new boolean[N][N];
+        uf = new WeightedQuickUnionUF(N * N + 2);
+        ufTop = new WeightedQuickUnionUF(N * N + 1);
+        visualTopPoint = N * N;
+        visualBottomPoint = N * N + 1;
+        for (int i = 0; i < N; i++) {
+            int num = xyTo1D(0, i);
+            uf.union(visualTopPoint, num);
+            ufTop.union(visualTopPoint, num);
+            num = xyTo1D(N - 1, i);
+            uf.union(visualBottomPoint, num);
+        }
+        numberOfOpenSites = 0;
+
+    }
+    public void open(int row, int col) {
+        if (!inTheRange(row, col)) {
+            throw new IllegalArgumentException("Arguments out of scope");
+        }
+        if (grid[row][col]) {
+            return;
+        }
+        grid[row][col] = true;
+        numberOfOpenSites++;
+        int[][] directions = {{1, 0}, {-1, 0} , {0, 1}, {0, -1}};
+        for (int[]dir : directions) {
+            int x = row + dir[0];
+            int y = col + dir[1];
+            if (inTheRange(x, y) && grid[x][y]) {
+                uf.union(xyTo1D(x, y), xyTo1D(row, col));
+                ufTop.union(xyTo1D(x, y), xyTo1D(row, col));
+
+            }
+        }
+
+    }
+    private boolean inTheRange(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < N;
+
+    }
+    public boolean isOpen(int row, int col) {
+        if (!inTheRange(row, col)) {
+            throw new IllegalArgumentException("Arguments out of scope");
+        }
+        return grid[row][col];
+
+    }
+    public boolean isFull(int row, int col) {
+        if (!inTheRange(row, col)) {
+            throw new IllegalArgumentException("Arguments out of scope");
+        }
+        int num = xyTo1D(row, col);
+        return isOpen(row, col) && ufTop.connected(visualTopPoint, num);
+    }
+    public int numberOfOpenSites() {
+        return numberOfOpenSites;
+    }
+    private int xyTo1D(int x, int y) {
+        return grid.length * x + y;
+    }
+    public boolean percolates() {
+        return uf.connected(visualTopPoint, visualBottomPoint);
+
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,61 @@
 package hw2;
 
+
+import edu.princeton.cs.algs4.StdRandom;
+
 public class PercolationStats {
+    int TIMES;
+    Percolation per;
+    int[] numberOfOpenSites;
+    double[] threshold;
+    double mean;
+    double stddev;
+
+
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        numberOfOpenSites = new int[T];
+        TIMES = T;
+        for (int i = 0; i < T; i++) {
+            per = pf.make(N);
+            while (!per.percolates()) {
+                int x = StdRandom.uniform(N);
+                int y = StdRandom.uniform(N);
+                per.open(x, y);
+            }
+            numberOfOpenSites[i] = per.numberOfOpenSites();
+            threshold[i] = (double) numberOfOpenSites[i] / (N * N);
+
+        }
+        mean = mean();
+        stddev = stddev();
+
+    }
+    public double mean() {
+        double total = 0;
+        for (double n : threshold) {
+            total += n;
+        }
+
+        return total / TIMES;
+    }
+    public double stddev() {
+        double total = 0;
+        for (double n : threshold) {
+            total += Math.pow((n - mean), 2);
+        }
+        return total / (TIMES - 1);
+
+    }
+    public double confidenceLow() {
+        return mean - (1.96 * Math.sqrt(stddev) / Math.sqrt(TIMES));
+
+    }
+    public double confidenceHigh() {
+        return mean + (1.96 * Math.sqrt(stddev) / Math.sqrt(TIMES));
+    }
 
 }
